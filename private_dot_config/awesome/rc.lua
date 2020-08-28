515a3fcc80cfcbb4a827bd8a47d68835ce400fde
jk <jk@vin.ovh>
2020-08-28 06:25:15 -0500

    updated dotfiles

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -120,9 +120,6 @@ end
 screen.connect_signal("property::geometry", set_wallpaper)
 
 awful.screen.connect_for_each_screen(function(s)
-    -- Wallpaper
-    set_wallpaper(s)
-
     -- Each screen has its own tag table.
     awful.tag({ " 1 ", " 2 ", " 3 ", " 4 ", " 5 ", " 6 ", " 7 ", " 8 ", " 9 " }, s, awful.layout.layouts[1])
 
