a9c05a75e705308da75be82a234351ad5e4e8634
jstkdng <jk@vin.ovh>
2021-10-16 23:07:01 -0500

    update

--- private_dot_config/awesome/startup.py
+++ private_dot_config/awesome/startup.py
@@ -4,6 +4,11 @@ import os
 from pathlib import Path
 
 def main():
+    log_file = Path.joinpath(Path.home(), Path(".local/share/xorg/Xorg.%s.log.old" % os.environ["DISPLAY"][-1]))
+    
+    if not log_file.exists():
+        return
+
     program_list = [
         # Core stuff
         "/usr/bin/picom",
@@ -23,6 +28,8 @@ def main():
         "/usr/bin/caffeine"
     ]
 
+    # Xorg.log.old file
+    log_file.unlink()
 
     for program in program_list:
         run(program)
