371bc3c6a52483357f6c86d8d53fa3e0ee52dbac
jstkdng <jk@vin.ovh>
2021-11-10 09:42:45 -0500

    update

--- private_dot_config/awesome/startup.py
+++ private_dot_config/awesome/startup.py
@@ -17,7 +17,8 @@ def main():
         "/usr/lib/geoclue-2.0/demos/agent",
         "/usr/bin/redshift-gtk",
         # Applets
-        "/usr/bin/pasystray -N none",
+        #"/usr/bin/pasystray -N none",
+        "/usr/bin/volctl",
         # General applications
         "/usr/bin/mpd",
         "/usr/bin/variety",
