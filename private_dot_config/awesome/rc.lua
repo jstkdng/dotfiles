b8c3f6caf8489c18834d0f15881701369a326d34
JustKidding <jk@vin.ovh>
2022-02-15 20:34:16 -0500

    update

--- private_dot_config/awesome/rc.lua
+++ private_dot_config/awesome/rc.lua
@@ -131,7 +131,7 @@ awful.screen.connect_for_each_screen(function(s)
     }
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s })
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 20 })
 
     -- Add widgets to the wibox
     s.mywibox:setup {
