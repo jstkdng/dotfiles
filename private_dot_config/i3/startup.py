5807fd4daa31514883fd2c670d54f28981f20c60
JustKidding <jk@vin.ovh>
2022-08-20 14:52:07 -0500

    u

---
+++ private_dot_config/i3/startup.py
@@ -0,0 +1,39 @@
+import subprocess
+import shlex
+import os
+from pathlib import Path
+
+def main():
+    program_list = [
+        # Core stuff
+        "/usr/bin/picom",
+        "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1",
+        str(Path.joinpath(Path.home(), Path(".config/awesome/lock"))),
+        "/usr/lib/geoclue-2.0/demos/agent",
+        "/usr/bin/redshift-gtk",
+        # General applications
+        "/usr/bin/mpd",
+        "/usr/bin/variety",
+        "/usr/bin/fcitx5",
+        "/usr/bin/nextcloud",
+        "/usr/bin/udiskie",
+        "/usr/bin/thunderbird",
+        "/usr/bin/caffeine",
+        "/usr/bin/cbatticon",
+        "/usr/bin/keepassxc"
+    ]
+
+    for program in program_list:
+        run(program)
+
+def run(program):
+    logname = program.split("/")[3].split(" ")[0]
+    log = open(Path.joinpath(Path.home(), Path(".local/share/xorg/%s.log" % logname)), "w")
+
+    try:
+        subprocess.Popen(shlex.split(program), stdout=log, stderr=log, shell=True)
+    except FileNotFoundError:
+        pass
+
+if __name__ == "__main__":
+    main()
