84ca839a4ef743bc44f521d67b1edec7933661bb
JustKidding <jk@vin.ovh>
2022-08-28 11:35:31 -0500

    u

---
+++ private_dot_config/i3/executable_startup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+run() {
+    args=("$@")
+    LOG=~/.local/share/xorg/$1.log
+    (${args[@]:1} &> $LOG)&
+}
+
+sleep 3
+
+run picom /usr/bin/picom --experimental-backends
+run thunderbird /usr/bin/thunderbird
+run polkit-gnome /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1
+run xss-lock /usr/bin/xss-lock -n /usr/lib/xsecurelock/dimmer -l -- /usr/bin/xsecurelock
+run geoclue /usr/lib/geoclue-2.0/demos/agent
+run redshift /usr/bin/redshift-gtk
+run mpd /usr/bin/mpd
+run variety /usr/bin/variety
+run fcitx5 /usr/bin/fcitx5
+run nextcloud /usr/bin/nextcloud
+run udiskie /usr/bin/udiskie
+run caffeine /usr/bin/caffeine
+run cbatticon /usr/bin/cbatticon
+run keepassxc /usr/bin/keepassxc
+run dunst /usr/bin/dunst
+
