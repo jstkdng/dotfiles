7c49846f17a1de410c602cce129cc281746cde18
JustKidding <jk@vin.ovh>
2022-09-25 00:24:07 -0500

    u

--- private_dot_config/i3/executable_polybar-launch.sh
+++ private_dot_config/i3/executable_polybar-launch.sh
@@ -6,8 +6,10 @@ polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
-# Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar mybar 2>&1 | tee -a /tmp/polybar1.log & disown
+log=/tmp/polybar-$USER.log
+
+# Launch mybar
+echo "---" | tee -a $log
+polybar mybar 2>&1 | tee -a $log & disown
 
 echo "Bars launched..."
