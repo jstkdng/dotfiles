0e297ba63be92884f56102eb4b6bddd24fb4012d
JustKidding <jk@vin.ovh>
2023-10-28 02:21:39 -0500

    remove old stuff

--- private_dot_config/nvim/ftplugin/java.lua
+++
@@ -1,5 +0,0 @@
-local config = {
-    cmd = {'/usr/bin/jdtls'},
-    root_dir = require('jdtls.setup').find_root({'.git', 'mvnw', 'gradlew'}),
-}
-require('jdtls').start_or_attach(config)
--- private_dot_config/nvim/lua/lualine-setup.lua
+++
@@ -1,6 +0,0 @@
-require('lualine').setup({
-    extensions = {'nvim-tree'},
-    options = {
-        theme = "material"
-    }
-})
