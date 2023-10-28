cd94c3315b8e8a0ff1a6d3c2041553c9e5eee3fb
JustKidding <jk@vin.ovh>
2023-10-28 02:23:27 -0500

    cleanup

--- dot_screenlayout/private_executable_2-screen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-xrandr --output eDP1 --mode 1366x768 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
-xrandr --output eDP1 --off
-variety -n
