4b1daec4381d5bca6bc2e1855ba66735d4c7aa93
JustKidding <jk@vin.ovh>
2023-10-27 20:46:56 -0500

    u

--- private_dot_config/i3/executable_polybar-launch.sh
+++ private_dot_config/i3/executable_polybar-launch.sh
@@ -7,9 +7,13 @@ polybar-msg cmd quit
 # killall -q polybar
 
 log=/tmp/polybar-$USER.log
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
 
 # Launch mybar
-echo "---" | tee -a "$log"
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m /usr/bin/polybar mybar 2>&1 | tee -a "$log" & disown
+MONITOR=$PRIMARY polybar mybar 2>&1 | tee -a "$log" & disown
+sleep 1
+
+for m in $OTHERS; do
+    MONITOR=$m polybar mybar 2>&1 | tee -a "$log" & disown
 done
