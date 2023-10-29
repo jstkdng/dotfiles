b642afb10d3642ea69e487a01c9b9785155fac36
JustKidding <jk@vin.ovh>
2023-10-29 15:30:17 -0500

    u

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,10 +4,10 @@ return {
     config = function(_, opts)
         require('nvim-treesitter.configs').setup({
             -- A list of parser names, or "all"
-            ensure_installed = { "c", "lua" },
+            ensure_installed = { "c", "lua", "bash" },
 
             -- Install parsers synchronously (only applied to `ensure_installed`)
-            -- sync_install = true,
+            sync_install = false,
 
             -- Automatically install missing parsers when entering buffer
             -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
